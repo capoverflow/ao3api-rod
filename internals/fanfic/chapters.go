85195dda5e935b7a6f3cb364b34db30e13dcd2b0
capoverflow <capoverflow@protonmail.ch>
2024-01-28 01:22:39 +0100

    Update GetFanfic function to wait for page idle state before getting HTML

---
+++ internals/fanfic/chapters.go
@@ -0,0 +1,46 @@
+package fanfic
+
+import (
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/capoverflow/ao3api-rod/internals/models"
+	"github.com/go-rod/rod"
+)
+
+func GetFanficChapters(work models.Work, page *rod.Page) (models.Work, error) {
+
+	html := page.MustWaitIdle().MustHTML()
+
+	os.WriteFile("test.html", []byte(html), 0644)
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		log.Println(err)
+	}
+
+	var chapters []models.Chapter
+
+	doc.Find("ol.chapter.index.group li").Each(func(i int, s *goquery.Selection) {
+		chapter := models.Chapter{}
+
+		str, _ := s.Find("a").Attr("href")
+		splitStr := strings.Split(str, "/")
+		chapter.ChapterID = splitStr[len(splitStr)-1]
+
+		chapter.Name = strings.TrimSpace(s.Find("a").Text())
+
+		dateStr := s.Find("span.datetime").Text()
+		dateStr = strings.Trim(dateStr, "()")
+		chapter.Date, _ = time.Parse("2006-01-02", dateStr)
+
+		chapters = append(chapters, chapter)
+	})
+
+	work.Chapters = chapters
+
+	return work, nil
+}
