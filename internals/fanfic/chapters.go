ce8a384c1d44e93ce755f195862852b0849f9ed5
capoverflow <capoverflow@protonmail.ch>
2024-01-28 22:17:42 +0100

    Add logic to retrieve work title and author if title is empty from the GetFanficChapters

--- internals/fanfic/chapters.go
+++ internals/fanfic/chapters.go
@@ -37,6 +37,22 @@ func GetFanficChapters(work models.Work, page *rod.Page) (models.Work, error) {
 		chapters = append(chapters, chapter)
 	})
 
+	// log.Println(work.Title)
+
+	if work.Title == "" {
+		log.Println("Title is empty")
+		doc.Find("h2.heading").Each(func(i int, el *goquery.Selection) {
+			el.Find("a").Each(func(z int, s *goquery.Selection) {
+				attr, _ := s.Attr("href")
+				if strings.Contains(attr, "works") {
+					work.Title = strings.TrimSpace(s.Text())
+				} else if strings.Contains(attr, "users") {
+					work.Author = append(work.Author, strings.TrimSpace(s.Text()))
+				}
+			})
+		})
+	}
+
 	work.Chapters = chapters
 
 	return work, nil
